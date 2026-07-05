87c82f060de872656d743f8e80f8114ed33cd7bf
Denis squidi <dk310ispwork@gmail.com>
2026-07-05 22:06:09 +0500

    .

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.country import Country
-from app.models.technology import Technology
-from app.models.user import User
-from app.models.case import Case, CaseStatus, TrustLevel
-from app.models.case_technology import CaseTechnology
-
